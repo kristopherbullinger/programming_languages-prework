04169af8066ff9baf7a661b6b7a5c291edb4044e
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-07-17 17:01:48 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,8 +4,9 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language_hash|
     language_hash.each do |language, information|
-      new_hash[language] = information
-      new_hash[language][:style] = []
+      new_hash[language] = information if new_hash[language].nil?
+      new_hash[language][:style] ||= []
+      new_hash[language][:style] << style
     end
   end
   new_hash.each do |language, information|
